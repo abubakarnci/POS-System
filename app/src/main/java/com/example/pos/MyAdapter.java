1e8429dec72e4a92e6f6fcec00ca86889f6d76c0
Abubakar <abubakargill538@gmail.com>
2021-01-06 17:54:49 +0000

    SalesActivity

--- app/src/main/java/com/example/pos/MyAdapter.java
+++ app/src/main/java/com/example/pos/MyAdapter.java
@@ -10,42 +10,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     Context context;
-    String data1[];
-    public MyAdapter(Context ct, String s1[]){
-        context=ct;
-        data1=s1;
+    ArrayList<InventObj> data1;
+    //String data1[];
+    public MyAdapter(Context ct, ArrayList<InventObj> s1){
+        this.context=ct;
+        this.data1=s1;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        LayoutInflater inflater=LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.my_row, parent,false);
+       // LayoutInflater inflater=LayoutInflater.from(context);
+       // View view = inflater.inflate(R.layout.my_row, parent,false);
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.my_row, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
 
-        holder.myText1.setText(data1[position]);
+        holder.item.setText(data1.get(i).getItemName());
+        holder.price.setText(String.valueOf(data1.get(i).getSellPrice()));
     }
 
     @Override
     public int getItemCount() {
-        return data1.length;
+        return data1.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        EditText myText1;
+        EditText item,price,qty;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            myText1=itemView.findViewById(R.id.name);
+            item=itemView.findViewById(R.id.item);
+            price=itemView.findViewById(R.id.price);
+            qty=itemView.findViewById(R.id.qty);
 
         }
     }
--- app/src/main/java/com/example/pos/SalesActivity.java
+++ app/src/main/java/com/example/pos/SalesActivity.java
@@ -1,25 +1,73 @@
 package com.example.pos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SalesActivity extends AppCompatActivity {
 
 
     RecyclerView recyclerView;
-    String s1[];
+
+    ArrayList<InventObj> input;
+
+    DatabaseReference retriveRef;
+    EditText name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
 
+        input=new ArrayList<>();
+        name=findViewById(R.id.editTextName);
         recyclerView= findViewById(R.id.recyclerView);
-        s1=getResources().getStringArray(R.array.test);
 
-        MyAdapter myAdapter=new MyAdapter(this, s1);
+
+
+        retriveRef = FirebaseDatabase.getInstance().getReference("items");
+
+        retriveRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                input.clear();
+                for(DataSnapshot dss : snapshot.getChildren()) {
+                    InventObj inventObj= dss.getValue(InventObj.class);
+                    inventObj.setItemNo(Long.parseLong(dss.getKey()));
+                   // String itemName = (String) snapshot.child("itemName").getValue();
+                    input.add(inventObj);
+                }
+
+
+                //    sellPrice = Double.parseDouble(String.valueOf(snapshot.child("sellPrice").getValue()));
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+
+        MyAdapter myAdapter=new MyAdapter(this, input);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
