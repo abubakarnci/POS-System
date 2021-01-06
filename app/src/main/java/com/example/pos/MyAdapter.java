d4bd8bdbf5346026bdd75170ed65a80d8b53cdfa
Abubakar <abubakargill538@gmail.com>
2021-01-06 01:02:14 +0000

    SalesActivity

---
+++ app/src/main/java/com/example/pos/MyAdapter.java
@@ -0,0 +1,52 @@
+package com.example.pos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    String data1[];
+    public MyAdapter(Context ct, String s1[]){
+        context=ct;
+        data1=s1;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.myText1.setText(data1[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        EditText myText1;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1=itemView.findViewById(R.id.name);
+
+        }
+    }
+}
